2b9805bdb9db0c9777c798a888f88cf4baf35d2d
isabellamoon <mjy12633@gmail.com>
2026-05-22 05:55:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function startListening() {
+      document.getElementById('status').innerText = '듣고 있습니다...';
+    }
+    function reset() {
+      document.getElementById('status').innerText = '음성으로 말씀하시면 바로 도와드립니다';
+    }
+    </script>
+    </body>
+</html>
