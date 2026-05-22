2352a26927ee9eda91a4231323fe71cc7818c569
isabellamoon <mjy12633@gmail.com>
2026-05-22 12:42:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function startListening() {
-      document.getElementById('status').innerText = '듣고 있습니다...';
-    }
-    function reset() {
-      document.getElementById('status').innerText = '음성으로 말씀하시면 바로 도와드립니다';
-    }
-    </script>
-    </body>
-</html>
+  // 상태 문구를 변경
+  document.getElementById('status').innerText = '듣고 있습니다...';
+}
+function reset() {
+  // 원래 문구로 되돌림
+  document.getElementById('status').innerText = '음성으로 말씀하시면 바로 도와드립니다';
+}
+function openComplaintGuide() {
+ window.location.href = "guide.html";
+}
