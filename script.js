0025fab4d59cf7ae5df50c4aed84c860514540e9
isabellamoon <mjy12633@gmail.com>
2026-05-23 13:35:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function reset() {
   // 원래 문구로 되돌림
   document.getElementById('status').innerText = '음성으로 말씀하시면 바로 도와드립니다';
 }
+//  말해U란 버튼
 function openComplaintGuide() {
- window.location.href = "guide.html";
+    window.location.href = "guide.html";
+}
+
+// 궁금해요 버튼
+function openGuide2() {
+    window.location.href = "guide2.html";
 }
