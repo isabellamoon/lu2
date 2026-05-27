e4546ed42b5cde0e62521ad9905d7b8aef4731c5
isabellamoon <mjy12633@gmail.com>
2026-05-27 14:52:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 function startListening() {
-  // 상태 문구를 변경
+
   document.getElementById('status').innerText = '듣고 있습니다...';
 }
 function reset() {
-  // 원래 문구로 되돌림
+
   document.getElementById('status').innerText = '음성으로 말씀하시면 바로 도와드립니다';
 }
-//  말해U란 버튼
+
 function openComplaintGuide() {
     window.location.href = "guide.html";
 }
 
-// 궁금해요 버튼
+
 function openGuide2() {
     window.location.href = "guide2.html";
+}
+
+
+function openGuide3() {
+    window.location.href = "guide3.html";
+}
+
+
+function openGuide4() {
+    window.location.href = "guide4.html";
+}
+
+
+function openGuide5() {
+    window.location.href = "guide5.html";
 }
